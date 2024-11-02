2fdb11498e24032f5da3919ceb44791ff4be797c
Somya Saxena <DesignedBySoumya@gmail.com>
2024-11-02 23:51:44 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,216 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Designer, Developer, and UI/UX Enthusiast.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Twinkling Candles - Brand Identity",
+    des: "Inspired by candlelight, this brand identity project captures elegance and ambiance.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://www.behance.net/gallery/182544241/Twinkling-Candles-Brand-Identity",
+  },
+  {
+    id: 2,
+    title: "Woman Accessories Branding",
+    des: "Crafted logo, brand identity, packaging, and website for the Payu.co brand.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://www.behance.net/gallery/148083887/Woman-Accessories-Branding",
+  },
+  {
+    id: 3,
+    title: "Goel Club - Brand Identity",
+    des: "Brand identity for Goel Club Plywood, blending natural beauty with craftsmanship.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://www.behance.net/gallery/182276863/Goel-club-Brand-Identity",
+  },
+  {
+    id: 4,
+    title: "Blissful Brews - Cafe Branding",
+    des: "Created a vibrant brand identity for Blissful Brews, a modern artisanal coffee brand.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://www.behance.net/gallery/123456789/Blissful-Brews-Cafe-Branding",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Soumya was a wonderful experience! He paid close attention to every detail, ensuring our vision was perfectly captured. His creativity, combined with his professionalism, resulted in branding that exceeded our expectations. I highly recommend Soumya for anyone looking to add a unique touch to their business.",
+    name: "Priya Verma",
+    title: "CEO of Verma Ventures",
+    img: "/profile1.svg",
+  },
+  {
+    quote:
+      "Soumya truly understands the essence of design! He brings a fresh perspective, yet remains rooted in traditional values. I was particularly impressed by his patience and willingness to make adjustments until everything felt just right. Soumya is a gem for anyone looking to elevate their brand.",
+    name: "Gopi Krishna Saladi",
+    title: "Founder of AQUAGENIXPRO",
+    img: "/profile2.svg",
+  },
+  {
+    quote:
+      "Exceptional work by Soumya! His designs reflect both elegance and modernity, and his approach is both thorough and thoughtful. Soumya doesn’t just work for you; he works with you, bringing your ideas to life with his creative flair. Highly recommended for top-notch design solutions!",
+    name: "Sneha Kapoor",
+    title: "Head of Marketing at Klean & Green",
+    img: "/profile3.svg",
+  },
+  {
+    quote:
+      "Soumya's dedication and expertise in design are commendable. I am thrilled with the branding he created for our company. His work ethic, combined with his attention to detail, made the whole process smooth and enjoyable. He has a knack for understanding exactly what the client wants!",
+    name: "Rahul Singh",
+    title: "Managing Director of Singh Industries",
+    img: "/profile4.svg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Brand Designer",
+    desc: "Crafted cohesive brand identities, ensuring visual consistency across various platforms and media.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "UI/UX Designer",
+    desc: "Designed intuitive user interfaces and seamless user experiences for web and mobile applications.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Frontend Developer",
+    desc: "Developed responsive and interactive web applications using React.js and  to create dynamic user experiences.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+
+  {
+    id: 4,
+    title: "Video & Motion Designer",
+    desc: "Created engaging motion graphics and video content, enhancing brand storytelling and engagement.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
